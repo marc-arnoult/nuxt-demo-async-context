c0833c51e7dd14749232efed9695fb9393233598
Arnoult <marc.arnoult@hotmail.fr>
2024-10-23 20:07:38 +0200

    Update correlationId.ts

--- server/middleware/correlationId.ts
+++ server/middleware/correlationId.ts
@@ -3,8 +3,8 @@ import { setCookie } from "#imports";
 
 export default defineEventHandler((event) => {
   // Add a random correlationId to the event context
-  const correlationId = randomUUID();
+  const correlationId = getCookie(event, "correlationId") || randomUUID();
   event.context.correlationId = correlationId;
 
-  setCookie("correlationId", correlationId);
+  setCookie(event, "correlationId", correlationId);
 });
