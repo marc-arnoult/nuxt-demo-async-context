9736d9f14166946b71e3e2ee8a7722312eb05bea
marc-arnoult <marc.arnoult@hotmail.fr>
2024-10-23 19:48:38 +0200

    Add correlationId in cookie

--- server/middleware/correlationId.ts
+++ server/middleware/correlationId.ts
@@ -1,6 +1,10 @@
 import { randomUUID } from 'crypto';
+import { setCookie } from "#imports";
 
 export default defineEventHandler((event) => {
   // Add a random correlationId to the event context
-  event.context.correlationId = randomUUID();
+  const correlationId = randomUUID();
+  event.context.correlationId = correlationId;
+
+  setCookie("correlationId", correlationId);
 });
